49afd6c88bf88fe844c52a319cc163b947d259f1
Pascualex <alejandropascualpozo@gmail.com>
2019-05-10 23:17:05 +0200

    Project moved to IntelliJ IDEA

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.pascualex.mazegenerator"},{"l":"com.github.pascualex.mazegenerator.maze"},{"l":"com.github.pascualex.mazegenerator.maze.components"},{"l":"com.github.pascualex.mazegenerator.swing"}]
